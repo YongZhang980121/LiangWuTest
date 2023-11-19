7de72f54641b14c0b5545c60b222a174a3e1c11c
zhang980121 <zhang980121@gmail.com>
2023-11-19 06:09:27 -0800

    enemy move

--- Assets/__Scripts/Enemy/Enemy.cs
+++ Assets/__Scripts/Enemy/Enemy.cs
@@ -1,14 +1,41 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
     public float health;
+    private Sequence wobbleSequence;
+    public Image spriteImage;
     private void OnEnable()
     {
         health = Global.enemyHp;
+        ResetAndStartWobbleAnimation();
+    }
+
+    private void OnDisable()
+    {
+        if (wobbleSequence != null)
+        {
+            wobbleSequence.Kill();
+        }
+    }
+    
+    private void ResetAndStartWobbleAnimation()
+    {
+        spriteImage.transform.localScale = Vector3.one;
+
+        if (wobbleSequence != null)
+        {
+            wobbleSequence.Kill();
+        }
+        wobbleSequence = DOTween.Sequence();
+        wobbleSequence.Append(spriteImage.transform.DOScale(new Vector3(1.1f, 0.9f, 1f), 0.5f))
+            .Append(spriteImage.transform.DOScale(new Vector3(0.9f, 1.1f, 1f), 0.5f))
+            .SetLoops(-1, LoopType.Yoyo);
     }
 
     public void TakeDamage(float damage)
